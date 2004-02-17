e04bf0645c6c86f3e28ff1ad29872cad635f3c8a
euges8 <euges8>
2004-02-17 21:15:38 +0000

    *** empty log message ***

---
+++ Euges-1.0/donnees/eugesSpem/EugesActivite.java
@@ -0,0 +1,186 @@
+ /*
+ * Created on 1 janv. 2004
+ *
+ */
+package donnees.eugesSpem;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import donnees.spem.process.structure.Activity;
+
+/**
+ * Cette classe représente définit une activité telle qu'elle sera représentée sous le logiciel Euges
+ * C'est à dire une activité définie par le SPEM avec des informations supplémentaires
+ * @author Nicolas Broueilh
+ */
+public class EugesActivite extends Activity{	
+
+	//vecteur d'activites realisees 
+	private Vector _activites = new Vector();
+	private Vector _roles = new Vector();
+	private Vector _produitsIn = new Vector();
+	private Vector _produitsOut = new Vector();
+
+	public EugesActivite () {
+		super();
+	}
+	
+	public EugesActivite (String name) {
+		super(name);
+	}
+
+	public String toString() {
+		return getName();
+	}
+	
+	public boolean ajouterActRealise(EugesActRealise a) {
+		if (!contientActRealise(a)) {
+			_activites.add(a);
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean supprimerActRealise(EugesActRealise a) {
+		if (contientActRealise(a)) {
+			_activites.remove(a);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean contientActRealise(EugesActRealise a) {
+		return _activites.contains(a);
+	}
+	
+	public int getActRealiseCount() {
+		return _activites.size();
+	}
+	/**
+	 * getIterationDebut()
+	 * @return le numero de l'iteration pendant laquelle commence l'activité
+	 */
+	public int getIterationDebut(){
+		if(getActRealiseCount()!=0){
+			int debut=((EugesActRealise)_activites.elementAt(0)).getIt();
+			for (Iterator iter = _activites.iterator(); iter.hasNext();) {
+				EugesActRealise element = (EugesActRealise) iter.next();
+				if(element.getIt()<debut)
+					debut=element.getIt();
+			}
+			return debut;
+		}
+		return -1;
+	}
+	/**
+	 * getIterationFin()
+	 * @return le numero de l'iteration pendant laquelle se termine l'activité
+	 */
+	public int getIterationFin(){
+		if(getActRealiseCount()!=0){
+			int fin=((EugesActRealise)_activites.elementAt(0)).getIt();
+			for (Iterator iter = _activites.iterator(); iter.hasNext();) {
+				EugesActRealise element = (EugesActRealise) iter.next();
+				if(element.getIt()>fin)
+					fin=element.getIt();
+			}
+			return fin;
+		}
+		return -1;
+	}
+	
+	public EugesActRealise getActRealise(int i) {
+		return (EugesActRealise) _activites.get(i);
+	}
+	
+
+	public boolean ajouterRole(EugesRole r) {
+		if (!contientRole(r)) {
+			_roles.add(r);
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean supprimerRole(EugesRole r) {
+		if (contientRole(r)) {
+			_activites.remove(r);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean contientRole(EugesRole r) {
+		return _roles.contains(r);
+	}
+
+	
+	public int getRoleCount() {
+			return _roles.size();
+		}
+
+	public EugesRole getRole(int i) {
+		return (EugesRole) _roles.get(i);
+	}
+	
+	public boolean ajouterProduitIn(EugesProduit a) {
+		if (!contientProduitIn(a)) {
+			_produitsIn.add(a);
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean supprimerProduitIn(EugesProduit a) {
+		if (contientProduitIn(a)) {
+			_produitsIn.remove(a);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean contientProduitIn(EugesProduit a) {
+		return _produitsIn.contains(a);
+	}
+
+	public int getProduitInCount() {
+		return _produitsIn.size();
+	}
+
+	public EugesProduit getProduitIn(int i) {
+		return (EugesProduit) _produitsIn.get(i);
+	}
+
+	public boolean ajouterProduitOut(EugesProduit a) {
+		if (!contientProduitOut(a)) {
+			_produitsOut.add(a);
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean supprimerProduitOut(EugesProduit a) {
+		if (contientProduitOut(a)) {
+			_produitsOut.remove(a);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean contientProduitOut(EugesProduit a) {
+		return _produitsOut.contains(a);
+	}
+
+	public int getProduitOutCount() {
+		return _produitsOut.size();
+	}
+
+	public EugesProduit getProduitOut(int i) {
+		return (EugesProduit) _produitsOut.get(i);
+	}
+}
---
+++ Euges-1.0/utilitaires/GestionImage.java
@@ -0,0 +1,171 @@
+/*
+ * Created on 24 janv. 2004
+ *
+ */
+package utilitaires;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * @author Mathieu GAYRAUD
+ *
+ */
+public class GestionImage {
+	public static Image _activite;
+	public static Image _actor;
+	public static Image _eclipse;
+	public static Image _euges;
+	public static Image _eugesAssistant;
+	public static Image _it;
+	public static Image _it_disabled;
+	public static Image _produit;
+	public static Image _role;
+	public static Image _aideBar;
+	public static Image _enregistrerBar;
+	public static Image _fermerBar;
+	public static Image _mailBar;
+	public static Image _nouveauBar;
+	public static Image _ouvrirBar;
+	public static Image _afficherAide;
+	public static Image _masquerAide;
+	public static Image _precedentAide;
+	public static Image _suivantAide;
+	public static Image _plus;
+	public static Image _moins;
+	public static Image _start;
+	public static Image _triActivite;
+	public static Image _triDefaut;
+	public static Image _triIteration;
+	public static Image _triRole;
+	public static Image _iconeCalendrier;
+	public static Image _navBack;
+	public static Image _navForward;
+	public static Image _navRefresh;
+	public static Image _navStop;
+	public static Image _navGo;
+	
+	public static Image _toolTabIt;
+	public static Image _toolGraphIt;
+	public static Image _toolGraphLine;
+	private URL url;
+	
+	private String _basePath = "/configuration/images/";
+	
+	public GestionImage(Display display) {
+		try {
+			url = getClass().getResource(_basePath + "activite.gif");
+			_activite = new Image(display,url.openStream());
+
+			url = getClass().getResource(_basePath + "actor.gif");
+			_actor = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "eclipse.png");
+			_eclipse = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "euges.ico");
+			_euges = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "eugesAssistant.png");
+			_eugesAssistant = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "it.gif");
+			_it = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "it_disabled.gif");
+			_it_disabled = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "iconesAide/afficher.ico");
+			_afficherAide = new Image(display,url.openStream());
+			
+//			url = getClass().getResource(_basePath + "icones/enregistrer.ico");
+//			_enregistrerBar = new Image(display,url.openStream());
+//			
+//			url = getClass().getResource(_basePath + "icones/fermer.ico");
+//			_fermerBar = new Image(display,url.openStream());
+//			
+//			url = getClass().getResource(_basePath + "icones/nouveau.ico");
+//			_nouveauBar = new Image(display,url.openStream());
+//			
+//			url = getClass().getResource(_basePath + "icones/ouvrir.ico");
+//			_ouvrirBar = new Image(display,url.openStream());
+//			
+//			url = getClass().getResource(_basePath + "icones/mail.ico");
+//			_mailBar = new Image(display,url.openStream());
+//			
+//			url = getClass().getResource(_basePath + "icones/aide.ico");
+//			_aideBar = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/tri_activite.ico");
+			_triActivite = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/tri_defaut.ico");
+			_triDefaut = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/tri_iteration.ico");
+			_triIteration = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/tri_role.ico");
+			_triRole = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "iconesAide/masquer.ico");
+			_masquerAide = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "iconesAide/précédent.ico");
+			_precedentAide = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "produit.gif");
+			_produit = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "role.gif");
+			_role = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "iconesAide/suivant.ico");
+			_suivantAide = new Image(display,url.openStream());
+
+			url = getClass().getResource(_basePath + "plus.gif");
+			_plus = new Image(display,url.openStream());
+
+			url = getClass().getResource(_basePath + "moins.gif");
+			_moins = new Image(display,url.openStream());
+			
+			//image popup pour le demarrage
+			url = getClass().getResource(_basePath + "start.jpg");
+			_start = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "calendrier.png");
+			_iconeCalendrier = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "navigatorGif/back.gif");
+			_navBack = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "navigatorGif/forward.gif");
+			_navForward = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "navigatorGif/go.gif");
+			_navGo = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "navigatorGif/refresh.gif");
+			_navRefresh = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "navigatorGif/stop.gif");
+			_navStop = new Image(display,url.openStream());
+	
+			url = getClass().getResource(_basePath + "icones/tabIt.ico");
+			_toolTabIt = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/graphIt.png");
+			_toolGraphIt = new Image(display,url.openStream());
+			
+			url = getClass().getResource(_basePath + "icones/graphLine.ico");
+			_toolGraphLine = new Image(display,url.openStream());
+			
+		} catch (IOException e) {
+			System.out.println("Erreur au chargement d'une image");
+			e.printStackTrace();
+		}
+	}
+}
